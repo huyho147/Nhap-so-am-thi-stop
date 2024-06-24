e8a31a46ea99d25fbd29df16bedd0daae15c9464
huyho147 <beobeosusu0511@gmail.com>
2024-06-24 19:12:24 +0700

    Add project files.

---
+++ Nhap so am thi stop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+
+	for (;;)
+	{
+		cout << "Nhap 1 so: ";
+		cin >> n;
+		if (n < 0)
+		{
+			cout << "Da nhap so am!!!";
+			break;
+		}
+			
+	}
+}
